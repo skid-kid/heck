fed943409afd98a6b21e7bcc6bb5d82fef05cacc
skid-kid <polarb587@gmail.com>
2024-09-11 01:08:51 +0530

    again minimal

--- heck/src/App.jsx
+++ heck/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='w-screen min-h-screen fixed  flex justify-center px-6 py-40  pointer-event-none'>
-        <div className="absolute inset-0 bg-[url('/griddy.svg')] bg-cover opacity-10"></div>
+        <div className="absolute inset-0 bg-[url('/griddy.svg')] bg-cover opacity-5"></div>
         <img src='/mesh.svg' 
         className='opacity-60 absolute h-[1000] w-1000' />
       </div>
